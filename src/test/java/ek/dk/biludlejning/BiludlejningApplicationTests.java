f9f0962635ae7e822dd9392bd062dc2bdf6a0c51
natfresco-DK <132372727+natfresco-DK@users.noreply.github.com>
2026-04-29 09:39:46 +0200

    Remove unused Spring Boot annotations from BiludlejningApplicationTests

--- src/test/java/ek/dk/biludlejning/BiludlejningApplicationTests.java
+++ src/test/java/ek/dk/biludlejning/BiludlejningApplicationTests.java
@@ -1,9 +1,6 @@
 package ek.dk.biludlejning;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
 
 class BiludlejningApplicationTests {
 
