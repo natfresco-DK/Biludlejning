435a7cc21d84bd94e845d4fbe8e73e8cbca62061
natfresco-DK <132372727+natfresco-DK@users.noreply.github.com>
2026-04-29 09:36:31 +0200

    Fix column names in rental_agreements and update test configuration

--- src/test/java/ek/dk/biludlejning/BiludlejningApplicationTests.java
+++ src/test/java/ek/dk/biludlejning/BiludlejningApplicationTests.java
@@ -1,9 +1,10 @@
 package ek.dk.biludlejning;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class BiludlejningApplicationTests {
 
     @Test
