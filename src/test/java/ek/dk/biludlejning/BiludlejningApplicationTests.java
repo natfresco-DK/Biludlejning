5cc6505e7c812173a085315f3bd9d48ccf55f076
natfresco-DK <132372727+natfresco-DK@users.noreply.github.com>
2026-04-30 09:47:44 +0200

    Add test database configuration and update test class for H2 support

--- src/test/java/ek/dk/biludlejning/BiludlejningApplicationTests.java
+++ src/test/java/ek/dk/biludlejning/BiludlejningApplicationTests.java
@@ -3,10 +3,12 @@ package ek.dk.biludlejning;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.ActiveProfiles;
 
+@Profile("test")
 @SpringBootTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class BiludlejningApplicationTests {
     @Test
     void contextLoads() {
