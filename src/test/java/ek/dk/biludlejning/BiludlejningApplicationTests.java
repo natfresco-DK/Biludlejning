74c67bf8027a27c2ddb8fb6d6922c1de6b4bdb1e
natfresco-DK <132372727+natfresco-DK@users.noreply.github.com>
2026-04-30 09:38:31 +0200

    Update test configuration to use AutoConfigureTestDatabase for better isolation

--- src/test/java/ek/dk/biludlejning/BiludlejningApplicationTests.java
+++ src/test/java/ek/dk/biludlejning/BiludlejningApplicationTests.java
@@ -1,15 +1,14 @@
 package ek.dk.biludlejning;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("test")
 @SpringBootTest
-class BiludlejningApplicationTests {
-
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+public class BiludlejningApplicationTests {
     @Test
     void contextLoads() {
     }
-
 }
