889ea01f3140b56ed9dc3d992058416f369ba067
10OliverF <ofolkersen@gmail.com>
2026-05-05 10:55:13 +0200

    Add logging to CarController for user access and car return events

--- src/main/java/ek/dk/biludlejning/controller/CarController.java
+++ src/main/java/ek/dk/biludlejning/controller/CarController.java
@@ -1,10 +1,11 @@
 package ek.dk.biludlejning.controller;
 
 import ek.dk.biludlejning.model.Car;
-import ek.dk.biludlejning.model.RentalAgreement;
 import ek.dk.biludlejning.model.User;
 import ek.dk.biludlejning.service.CarService;
 import ek.dk.biludlejning.service.RentalAgreementService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,13 +23,15 @@ public class CarController {
     private final CarService carService;
     private final RentalAgreementService rentalAgreementService;
 
+    private static final Logger logger = LoggerFactory.getLogger(CarController.class);
+
     public CarController(CarService carService, RentalAgreementService rentalAgreementService) {
         this.carService = carService;
         this.rentalAgreementService = rentalAgreementService;
     }
 
     @GetMapping("/cars")
-    public  String cars(Model model,
+    public String cars(Model model,
                                  @SessionAttribute(name = "currentUser", required = false) User currentUser) {
         model.addAttribute("currentUser", currentUser);
         List<Car> cars = carService.getAllCars();
@@ -40,12 +43,14 @@ public class CarController {
         }
         model.addAttribute("leaseCompletedMap",leaseCompletedMap);
         model.addAttribute("activePage", "cars");
+        logger.info("User with User id={} and email={} accessed /cars", (currentUser != null ? currentUser.getId() : "null"), (currentUser != null ? currentUser.getEmail() : "null"));
         return "cars";
     }
 
     @PostMapping("/car-return/{carId}")
-    public String returnCar(@PathVariable int carId) {
+    public String returnCar(@PathVariable int carId, @SessionAttribute(name = "currentUser", required = false) User currentUser) {
         rentalAgreementService.setCarAsReturned(carId);
+        logger.info("Car with id={} has been marked as returned by User id={}", carId, currentUser.getId());
         return "redirect:/cars";
     }
 
