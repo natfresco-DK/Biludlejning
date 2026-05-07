e6a44da54e33d9bae909b205cc3b1074a37dbc3b
Simon Nat Rignel Fresco <93189144+Natfresco@users.noreply.github.com>
2026-05-07 11:15:40 +0200

    Refactor DamageController imports to streamline code structure

--- src/main/java/ek/dk/biludlejning/controller/DamageController.java
+++ src/main/java/ek/dk/biludlejning/controller/DamageController.java
@@ -9,13 +9,8 @@ import ek.dk.biludlejning.service.RentalAgreementService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import java.util.Optional;
 
